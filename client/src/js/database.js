4c2d8c399732a3a8bad49858f1073227477dafb5
Zane Pisano <pisanoz@live.com>
2022-07-13 00:19:57 -0700

    fixing sw

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,30 +1,54 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+	openDB("jate", 1, {
+		upgrade(db) {
+			if (db.objectStoreNames.contains("jate")) {
+				console.log("jate database already exists");
+				return;
+			}
+			db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+			console.log("jate database created");
+		},
+	});
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const postDb =async (content)=>{
-  console.log('post to Db');
+export const postDb = async (content) => {
+	console.log("post to Db");
 
-  const jateDb = await openDB ('jate', 1);
+	const jateDb = await openDB("jate", 1);
 
-  const tx = jateDb.transaction ('jate', 'readwright');
+	const tx = jateDb.transaction("jate", "readwright");
 
-  const 
-}
-export const putDb = async (content) => console.error('putDb not implemented');
+	const store = tx.objectStore("jate");
+
+	const request = store.add({ content: content });
+
+	const result = await request;
+	console.log("Data saved to database", result);
+};
+export const putDb = async (content) => {};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async (content) => {
+	console.log("GET from the database");
+
+	// Create a connection to the database database and version we want to use.
+	const contactDb = await openDB("jate", 1);
+
+	// Create a new transaction and specify the database and data privileges.
+	const tx = contactDb.transaction("content", "readonly");
+
+	// Open up the desired object store.
+	const store = tx.objectStore("content");
+
+	// Use the .getAll() method to get all data in the database.
+	const request = store.getAll();
+
+	// Get confirmation of the request.
+	const result = await request;
+	console.log(result);
+	return result;
+};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
@@ -21,11 +21,12 @@ module.exports = () => {
 			new HtmlWebpackPlugin({
 				template: "./index.html",
 			}),
-			new MiniCssExtractPlugin(),
+
 			new InjectManifest({
 				swSrc: "./src/sw.js",
 				swDest: "service-worker.js",
 			}),
+			new MiniCssExtractPlugin(),
 		],
 
 		module: {
